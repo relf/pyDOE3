7bd687e6d3e22dbbc18eab14463e329be7d74b74
RickardSjogren <r.sjogren89@gmail.com>
2018-01-26 10:49:51 +0100

    Added argument for passing in random-state/seed into LHS-designs. Also, fixed doctest:s.

--- pyDOE2/doe_lhs.py
+++ pyDOE2/doe_lhs.py
@@ -19,114 +19,127 @@ from scipy import spatial
 __all__ = ['lhs']
 
 
-def lhs(n, samples=None, criterion=None, iterations=None):
+def lhs(n, samples=None, criterion=None, iterations=None, random_state=None):
     """
     Generate a latin-hypercube design
-    
+
     Parameters
     ----------
     n : int
         The number of factors to generate samples for
-    
+
     Optional
     --------
     samples : int
         The number of samples to generate for each factor (Default: n)
     criterion : str
-        Allowable values are "center" or "c", "maximin" or "m", 
-        "centermaximin" or "cm", and "correlation" or "corr". If no value 
+        Allowable values are "center" or "c", "maximin" or "m",
+        "centermaximin" or "cm", and "correlation" or "corr". If no value
         given, the design is simply randomized.
     iterations : int
         The number of iterations in the maximin and correlations algorithms
         (Default: 5).
-    
+    randomstate : np.random.RandomState, int
+         Random state (or seed-number) which controls the seed and random draws
+
     Returns
     -------
     H : 2d-array
         An n-by-samples design matrix that has been normalized so factor values
         are uniformly spaced between zero and one.
-    
+
     Example
     -------
     A 3-factor design (defaults to 3 samples)::
-    
-        >>> lhs(3)
-        array([[ 0.40069325,  0.08118402,  0.69763298],
-               [ 0.19524568,  0.41383587,  0.29947106],
-               [ 0.85341601,  0.75460699,  0.360024  ]])
-       
+
+        >>> lhs(3, random_state=42)
+        array([[ 0.12484671,  0.95539205,  0.24399798],
+               [ 0.53288616,  0.38533955,  0.86703834],
+               [ 0.68602787,  0.31690477,  0.38533151]])
+
     A 4-factor design with 6 samples::
 
-        >>> lhs(4, samples=6)
-        array([[ 0.27226812,  0.02811327,  0.62792445,  0.91988196],
-               [ 0.76945538,  0.43501682,  0.01107457,  0.09583358],
-               [ 0.45702981,  0.76073773,  0.90245401,  0.18773015],
-               [ 0.99342115,  0.85814198,  0.16996665,  0.65069309],
-               [ 0.63092013,  0.22148567,  0.33616859,  0.36332478],
-               [ 0.05276917,  0.5819198 ,  0.67194243,  0.78703262]])
-       
+        >>> lhs(4, samples=6, random_state=42)
+        array([[ 0.06242335,  0.19266575,  0.88202411,  0.89439364],
+               [ 0.19266977,  0.53538985,  0.53030416,  0.49498498],
+               [ 0.71737371,  0.75412607,  0.17634727,  0.71520486],
+               [ 0.63874044,  0.85658231,  0.33676408,  0.31102936],
+               [ 0.43351917,  0.45134543,  0.12199899,  0.53056742],
+               [ 0.93530882,  0.15845238,  0.7386575 ,  0.09977641]])
+
     A 2-factor design with 5 centered samples::
-    
-        >>> lhs(2, samples=5, criterion='center')
-        array([[ 0.3,  0.5],
-               [ 0.7,  0.9],
-               [ 0.1,  0.3],
-               [ 0.9,  0.1],
-               [ 0.5,  0.7]])
-       
+
+        >>> lhs(2, samples=5, criterion='center', random_state=42)
+        array([[ 0.1,  0.9],
+               [ 0.5,  0.5],
+               [ 0.7,  0.1],
+               [ 0.3,  0.7],
+               [ 0.9,  0.3]])
+
     A 3-factor design with 4 samples where the minimum distance between
     all samples has been maximized::
-    
-        >>> lhs(3, samples=4, criterion='maximin')
-        array([[ 0.02642564,  0.55576963,  0.50261649],
-               [ 0.51606589,  0.88933259,  0.34040838],
-               [ 0.98431735,  0.0380364 ,  0.01621717],
-               [ 0.40414671,  0.33339132,  0.84845707]])
-       
+
+        >>> lhs(3, samples=4, criterion='maximin', random_state=42)
+        array([[ 0.69754389,  0.2997106 ,  0.96250964],
+               [ 0.10585037,  0.09872038,  0.73157522],
+               [ 0.25351996,  0.65148999,  0.07337204],
+               [ 0.91276926,  0.97873992,  0.42783549]])
+
     A 4-factor design with 5 samples where the samples are as uncorrelated
     as possible (within 10 iterations)::
-    
-        >>> lhs(4, samples=5, criterion='correlate', iterations=10)
-    
+
+        >>> lhs(4, samples=5, criterion='correlation', iterations=10, random_state=42)
+        array([[ 0.72088348,  0.05121366,  0.97609357,  0.92487081],
+               [ 0.49507404,  0.51265511,  0.00808672,  0.37915272],
+               [ 0.22217816,  0.2878673 ,  0.24034384,  0.42786629],
+               [ 0.91977309,  0.93895699,  0.64061224,  0.14213258],
+               [ 0.04719698,  0.70796822,  0.53910322,  0.78857071]])
+
     """
     H = None
-    
+
+    if random_state is None:
+        random_state = np.random.RandomState()
+    elif not isinstance(random_state, np.random.RandomState):
+        random_state = np.random.RandomState(random_state)
+
     if samples is None:
         samples = n
-    
+
     if criterion is not None:
-        assert criterion.lower() in ('center', 'c', 'maximin', 'm', 
-            'centermaximin', 'cm', 'correlation', 
-            'corr'), 'Invalid value for "criterion": {}'.format(criterion)
+        if not criterion.lower() in ('center', 'c', 'maximin', 'm',
+                                     'centermaximin', 'cm', 'correlation',
+                                     'corr'):
+            raise ValueError('Invalid value for "criterion": {}'.format(criterion))
+
     else:
-        H = _lhsclassic(n, samples)
+        H = _lhsclassic(n, samples, random_state)
 
     if criterion is None:
         criterion = 'center'
-    
     if iterations is None:
         iterations = 5
-        
+
     if H is None:
         if criterion.lower() in ('center', 'c'):
-            H = _lhscentered(n, samples)
+            H = _lhscentered(n, samples, random_state)
         elif criterion.lower() in ('maximin', 'm'):
-            H = _lhsmaximin(n, samples, iterations, 'maximin')
+            H = _lhsmaximin(n, samples, iterations, 'maximin', random_state)
         elif criterion.lower() in ('centermaximin', 'cm'):
-            H = _lhsmaximin(n, samples, iterations, 'centermaximin')
-        elif criterion.lower() in ('correlate', 'corr'):
-            H = _lhscorrelate(n, samples, iterations)
-    
+            H = _lhsmaximin(n, samples, iterations, 'centermaximin', random_state)
+        elif criterion.lower() in ('correlation', 'corr'):
+            H = _lhscorrelate(n, samples, iterations, random_state)
+
     return H
 
 ################################################################################
 
-def _lhsclassic(n, samples):
+def _lhsclassic(n, samples, randomstate):
     # Generate the intervals
     cut = np.linspace(0, 1, samples + 1)    
     
     # Fill points uniformly in each interval
-    u = np.random.rand(samples, n)
+    u = randomstate.rand(samples, n)
     a = cut[:samples]
     b = cut[1:samples + 1]
     rdpoints = np.zeros_like(u)
@@ -136,19 +149,19 @@ def _lhsclassic(n, samples):
     # Make the random pairings
     H = np.zeros_like(rdpoints)
     for j in range(n):
-        order = np.random.permutation(range(samples))
+        order = randomstate.permutation(range(samples))
         H[:, j] = rdpoints[order, j]
     
     return H
     
 ################################################################################
 
-def _lhscentered(n, samples):
+def _lhscentered(n, samples, randomstate):
     # Generate the intervals
     cut = np.linspace(0, 1, samples + 1)    
     
     # Fill points uniformly in each interval
-    u = np.random.rand(samples, n)
+    u = randomstate.rand(samples, n)
     a = cut[:samples]
     b = cut[1:samples + 1]
     _center = (a + b)/2
@@ -156,21 +169,21 @@ def _lhscentered(n, samples):
     # Make the random pairings
     H = np.zeros_like(u)
     for j in range(n):
-        H[:, j] = np.random.permutation(_center)
+        H[:, j] = randomstate.permutation(_center)
     
     return H
     
 ################################################################################
 
-def _lhsmaximin(n, samples, iterations, lhstype):
+def _lhsmaximin(n, samples, iterations, lhstype, randomstate):
     maxdist = 0
     
     # Maximize the minimum distance between points
     for i in range(iterations):
         if lhstype=='maximin':
-            Hcandidate = _lhsclassic(n, samples)
+            Hcandidate = _lhsclassic(n, samples, randomstate)
         else:
-            Hcandidate = _lhscentered(n, samples)
+            Hcandidate = _lhscentered(n, samples, randomstate)
         
         d = spatial.distance.pdist(Hcandidate, 'euclidean')
         if maxdist<np.min(d):
@@ -181,17 +194,16 @@ def _lhsmaximin(n, samples, iterations, lhstype):
 
 ################################################################################
 
-def _lhscorrelate(n, samples, iterations):
+def _lhscorrelate(n, samples, iterations, randomstate):
     mincorr = np.inf
     
     # Minimize the components correlation coefficients
     for i in range(iterations):
         # Generate a random LHS
-        Hcandidate = _lhsclassic(n, samples)
+        Hcandidate = _lhsclassic(n, samples, randomstate)
         R = np.corrcoef(Hcandidate)
         if np.max(np.abs(R[R!=1]))<mincorr:
             mincorr = np.max(np.abs(R-np.eye(R.shape[0])))
-            print('new candidate solution found with max,abs corrcoef = {}'.format(mincorr))
             H = Hcandidate.copy()
     
     return H
