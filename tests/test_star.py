7f08a7c240cbaea8250eab9ee3fcce79c0a4153a
Antoine AVERLAND <antoine.averland@onera.fr>
2023-11-06 09:20:57 +0100

    pytest doe_star

---
+++ tests/test_star.py
@@ -0,0 +1,25 @@
+import unittest
+import array as array
+from pyDOE3.doe_star import star
+import numpy as np
+
+
+class Test_star(unittest.TestCase):
+    def test_star1(self):
+        expected = (
+            np.array(
+                [
+                    [-1.0, 0.0, 0.0],
+                    [1.0, 0.0, 0.0],
+                    [0.0, -1.0, 0.0],
+                    [0.0, 1.0, 0.0],
+                    [0.0, 0.0, -1.0],
+                    [0.0, 0.0, 1.0],
+                ]
+            ),
+            1,
+        )
+        actual = star(3)
+        print(actual)
+        np.testing.assert_allclose(actual[0], expected[0])
+        self.assertEqual(actual[1], expected[1])
